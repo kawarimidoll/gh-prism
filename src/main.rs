c4355ee33c73434079add8f69d46a8f90e6178d2
kawarimidoll <kawarimidoll+git@gmail.com>
2025-12-10 13:23:43 +0900

    feat: init main.rs with basic ratatui setup

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,25 @@
-fn main() {
-    println!("Hello, prism! 🔷");
+use color_eyre::Result;
+use crossterm::event::{self, Event};
+use ratatui::{DefaultTerminal, Frame};
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
+    let terminal = ratatui::init();
+    let result = run(terminal);
+    ratatui::restore();
+    result
+}
+
+fn run(mut terminal: DefaultTerminal) -> Result<()> {
+    loop {
+        terminal.draw(render)?;
+        if matches!(event::read()?, Event::Key(_)) {
+            break Ok(());
+        }
+    }
+}
+
+fn render(frame: &mut Frame) {
+    frame.render_widget("hello world", frame.area());
     println!("Arguments: {:?}", std::env::args().collect::<Vec<_>>());
 }
